ceecfd736cf4e4ee6331dbf5024044c6898ec224
Daniel Radding <dradding@gmail.com>
2014-11-26 14:54:38 -0800

    Updated to reflect clarifications

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-Ripple Balance Aggregator
+#Ripple Balance Aggregator
 
-Implementation:
-###
-I know Ripple is a JS/Node shop so I decided to build this in Node. Using Ripple Lib was a breeze between the documentation on Github, the WebSocket Tool, and the Ripple Charts Graph Tool. Additionally I didn't want to hardcode in any wallet addresses so I found a prompy module to allow the using to enter in the wallet address.
+###Implementation:
 
-Explanation:
-###
+I know Ripple is a JS/Node shop so I decided to build this in Node. Using Ripple Lib was a breeze between the documentation on Github, the WebSocket Tool, and the Ripple Charts Graph Tool. Additionally I didn't want to hardcode in any wallet addresses so I found a prompt module to allow the users to enter in the wallet address.
+
+###Explanation:
+
 To get the XRP balance I simply pull the balance out of the JSON returned from the requestAccountInfo call.
 
-To get the other balances was a little more work. Once I got the JSON response from requestAccountLines call I had to sort through the trustlines. I did this by maintaining an associative array that could be updated and added to. The key for each entry was a currency abbreviation and the value was the sum of IOUs given and recieved in that currency.
+To get the other balances was a little more work. Once I got the JSON response from requestAccountLines call I had to sort through the trustlines. I did this by maintaining an associative array that could be updated and added to for both the individual wallet as well as the aggregate balance. The key for each entry was a currency abbreviation and the value was the sum of IOUs given and recieved in that currency.
 
 I also put in some basic error handling. All errors are caught and handled so the program does not crash. Additionally when the error is from Ripple-Lib, the error_message from the err JSON is printed. I also set the prompt module to only accept alphanumeric characters.
 
-Time Spent:
-###
-Total time spend was around 3 hours. I didn't know Javascript or Node so I spent a lot of time learning syntax and conventions. Working with the API and writing the logic was probably the easiest part.
+I did have some issues with negative floats. However it seems widely accepted that floats have issues in js.
+
+###Time Spent:
+
+Total time spend was around 3-4 hours. I don't know Javascript or Node fluently so I spent a lot of time learning syntax and conventions. Working with the API and writing the logic was probably the easiest part.
--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 var Remote = require('ripple-lib').Remote;
 var prompt = require('prompt');
 
-var XRP = 0; //XRP balance
-var balances = {}; //Cumulative IOU balances
+var total_XRP = 0; //Cumulative XRP balance
+var total_balances = {}; //Cumulative IOU balances
+var XRP = 0; //XRP balance for the current wallet
+var balances = {}; //Cumulative IOU balances for the current wallet
 
 var properties = [
     {
@@ -13,7 +15,7 @@ var properties = [
     }
   ];
 
-console.log('Welcome to the Ripple Wallet Balance Aggregator. Enter \'exit\' to exit. \nCreated by Daniel Radding \n*************************');
+console.log('Welcome to the Ripple Wallet Balance Aggregator. Enter \'clear\' to start over. Enter \'exit\' to exit. \nCreated by Daniel Radding \n*************************');
 
 startPrompt(); //Use prompt to ask for wallet addresses
 
@@ -26,6 +28,12 @@ function startPrompt(){
 	  	console.log('The was an error caused by the \'prompt\' module.')
 	  	startPrompt();
 	  	return; }
+	  else if (result.wallet.toLowerCase() === 'clear'){
+	  	console.log('Aggregate balances cleared');
+	  	total_XRP = 0;
+	  	total_balances = {}
+	  	startPrompt();
+	  }
 	  else if (result.wallet.toLowerCase() === 'exit'){
 	  	process.exit();
 	  } else {
@@ -51,12 +59,12 @@ function getBalances(wallet){ //This function actually calls Ripple-Lib to get i
 	 		  startPrompt();
 	  		return; 
 	  	}
-	 	//console.log(info)
-	 	XRP = info.account_data.Balance; //Set the XRP value
+	  	XRP = parseFloat(info.account_data.Balance); //Set the XRP value
+	 		total_XRP += parseFloat(info.account_data.Balance); //Set the XRP value
 	  });
 	  //Next get IOU balances by process account_lines
 	  var request2 = remote.requestAccountLines(options, function(err, info) {
-	  	balances = {} //Clear balances from any previous uses
+	  	balances = {} //Clear balances from previous wallet balance
 	  	if (err) { 
 	  		return; //It is *extremely* unlikely the network will go out between request1 and request2.
 	  	}
@@ -65,10 +73,18 @@ function getBalances(wallet){ //This function actually calls Ripple-Lib to get i
 		 		//if currency in balances, update
 		 		currency = info.lines[i].currency;
 		 		balance = info.lines[i].balance;
-		 		if (currency in balances) {
+		 		if (currency in balances) { //Update individual wallet balance and aggregate balance
 		 			balances[currency] += parseFloat(balance);
-		 		} else { //Otherwise add the currency to balances
-		 			balances[currency] = parseFloat(balance);
+		 			total_balances[currency] += parseFloat(balance);
+		 		} else { //Otherwise add the currency to balances and check to see if already in total_balances
+		 			if (! (currency in total_balances)){
+		 				balances[currency] = parseFloat(balance);
+		 				total_balances[currency] = parseFloat(balance);
+		 			}
+		 			else{
+		 				balances[currency] = parseFloat(balance);
+		 				total_balances[currency] += parseFloat(balance);
+		 			}
 		 		}
 	 	}
 		console.log('Balances for wallet ' + options.account + ':' 
@@ -76,6 +92,15 @@ function getBalances(wallet){ //This function actually calls Ripple-Lib to get i
 	 	for (key in balances){
 	 		console.log(key + ": " + balances[key])
 	 	}
+	 	console.log('--------------------')
+	 	console.log('Total balances:' 
+		+ '\n' + 'XRP: ' + total_XRP);
+	 	for (key in total_balances){
+	 		console.log(key + ": " + total_balances[key])
+	 	}
+	 	
+	 	console.log("\n"); //for readability
+
 	 	startPrompt();
 	  });
 	});
